bebbcd06533f42a7cb1517f1733f329f48dff3dc
RawHav0kk <mn58835@tutanota.com>
2024-08-26 13:17:30 +0200

    Modified metadata

--- src/main/java/com/chud/ntm/Main.java
+++ src/main/java/com/chud/ntm/Main.java
@@ -1,4 +1,24 @@
 package com.chud.ntm;
 
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+import static com.chud.ntm.RefStrings.*;
+
+@Mod(modid = MODID, version = VERSION, name = NAME, acceptedMinecraftVersions = "[1.12.2]")
 public class Main {
+
+    public static final Logger LOGGER = LogManager.getLogger(MODID);
+
+    @EventHandler
+    public void preLoad(FMLPreInitializationEvent event){
+
+        LOGGER.warn("You are running Chud NTM: " + VERSION);
+
+    }
+
 }
