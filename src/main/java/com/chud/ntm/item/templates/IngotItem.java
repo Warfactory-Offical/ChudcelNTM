07ae812355e2d203a54f93ca45d65e404f51134c
Jakub Dolecki <doleckijakub@users.noreply.github.com>
2024-08-26 17:34:07 +0200

    Implemented OreDict for IngotItem

--- src/main/java/com/chud/ntm/item/templates/IngotItem.java
+++ src/main/java/com/chud/ntm/item/templates/IngotItem.java
@@ -1,20 +1,31 @@
 package com.chud.ntm.item.templates;
 
+import com.chud.api.IPossiblyHazardous;
 import com.chud.ntm.item.enums.MaterialNTM;
-import net.minecraft.creativetab.CreativeTabs;
+import com.chud.ntm.manager.CreativeTabsManager;
 import net.minecraft.item.Item;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.oredict.OreDictionary;
 
 import static com.chud.ntm.RefStrings.MODID;
 
-public class IngotItem extends Item {
+public class IngotItem extends Item implements IPossiblyHazardous {
 
     public IngotItem(MaterialNTM material) {
-        final String itemName = "ingot_" + material.toString();
+        final String material_xxx = material.toString().toLowerCase();
+        final String material_Xxx = material.toString().toUpperCase().charAt(0) + material_xxx.substring(1);
+
+        final String itemName = "ingot_" + material_xxx;
 
         setTranslationKey(itemName);
         setRegistryName(new ResourceLocation(MODID, itemName));
-        setCreativeTab(CreativeTabs.MATERIALS); // TODO: custom
+        setCreativeTab(CreativeTabsManager.partsTab);
+
+        OreDictionary.registerOre("ingot" + material_Xxx, this);
     }
 
+    @Override
+    public float getHazardMultiplier() {
+        return IPossiblyHazardous.super.getHazardMultiplier();
+    }
 }
