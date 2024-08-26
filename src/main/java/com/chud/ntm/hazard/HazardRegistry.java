ebfb2e2bd3b5a999e51e3a3668ee08e1812d4982
RawHav0kk <mn58835@tutanota.com>
2024-08-26 19:37:31 +0200

    added shape multipliers

--- src/main/java/com/chud/ntm/hazard/HazardRegistry.java
+++ src/main/java/com/chud/ntm/hazard/HazardRegistry.java
@@ -15,12 +15,41 @@ public class HazardRegistry {
     private static final Map<String, Float> shapeMultipliers = new LinkedHashMap<>();
 
     static {
-        shapeMultipliers.put("nugget", 0.1f);
-        shapeMultipliers.put("ingot", 1.f);
-        shapeMultipliers.put("gem", 1.f);
-        shapeMultipliers.put("plate", 1.f);
-        shapeMultipliers.put("powder", 3.f);
-        shapeMultipliers.put("block", 10.f);
+        final float nugget = 0.1F;
+        final float wire = 0.2F;
+        final float rod = 0.5F;
+        final float stick = 0.5F;
+        final float billet = 0.5F;
+        final float ingot = 1.0F;
+        final float gem = 1.0F;
+        final float powderMult = 3.0F;
+        final float block = 10.0F;
+        final float plate = ingot;
+        final float plateCast = plate * 3; //IDK what that is
+        final float powder = ingot * powderMult;
+        final float powderTiny = nugget * powderMult;
+        final float ore = ingot * 3;
+        final float crystal = block;
+        final float rtg = billet * 3;
+        final float rodDual = rod * 2;
+        final float rodQuad = rod * 4;
+        final float rodRbmk = rod * 8;
+
+        shapeMultipliers.put("nugget",  nugget);
+        shapeMultipliers.put("wire",    wire);
+        shapeMultipliers.put("stick",   stick);
+        shapeMultipliers.put("ingot",   ingot);
+        shapeMultipliers.put("gem",     gem);
+        shapeMultipliers.put("plate",   plate);
+        shapeMultipliers.put("powder",  powder);
+        shapeMultipliers.put("block",   powderTiny);
+        shapeMultipliers.put("crystal", crystal);
+        shapeMultipliers.put("rod",     rod);
+        shapeMultipliers.put("rodDual", rodDual);
+        shapeMultipliers.put("rodQuad", rodQuad);
+        shapeMultipliers.put("rodRbmk", rodRbmk);
+        shapeMultipliers.put("rtg",     rtg);
+        shapeMultipliers.put("ore",     ore);
     }
 
     private static final Map<MaterialNTM, List<Hazard>> materialHazards = new LinkedHashMap<>();
