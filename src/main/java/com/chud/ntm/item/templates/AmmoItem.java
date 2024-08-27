f74a4eddc6d01c83025c888f99d4b74431782955
Jakub Dolecki <doleckijakub@users.noreply.github.com>
2024-08-27 12:03:27 +0200

    Implemented ammo

---
+++ src/main/java/com/chud/ntm/item/templates/AmmoItem.java
@@ -0,0 +1,11 @@
+package com.chud.ntm.item.templates;
+
+import com.chud.ntm.manager.CreativeTabsManager;
+
+public class AmmoItem extends CustomItem {
+
+    public AmmoItem(String name) {
+        super(name, CreativeTabsManager.weaponTab);
+    }
+
+}
